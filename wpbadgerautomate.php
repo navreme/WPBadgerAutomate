03b7cdf6b4088b58aa1d1c5f9204db24e1e4a9ed
Jiri Pavlicek <jiri@pavlicek.cz>
2013-03-19 09:33:33 +0100

    first commit

---
+++ wpbadgerautomate.php
@@ -0,0 +1,21 @@
+<?php
+/**
+ * @package WPBadgerAutomate
+ * @version 1.6
+ */
+/*
+Plugin Name: WPBadgerAutomate
+Plugin URI: 
+Description: 
+Author: Navreme Bohemia
+Version: 0.1
+Author URI: http://www.navreme.cz/
+*/
+
+function nothing_yet() {
+
+}
+
+// add_action( 'admin_notices', 'nothing_yet' );
+
+?>
